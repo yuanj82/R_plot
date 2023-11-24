55403576772b12c29af7f8ba54fbc9b3904e7fd3
UncleCAT4 <hieroglyphs@foxmail.com>
2023-11-24 14:53:18 +0800

    update

--- GO/go.r
+++ GO/go.r
@@ -1,6 +1,9 @@
 library(stats) # Arch中需要先加载这些包
 library(base)
+library(showtext)
 
+font_add("Times_New_Roman", "/usr/share/fonts/truetype/msttcorefonts/Times_New_Roman.ttf")  # 添加新罗马字体
+showtext_auto()
 # Attaching package: ‘dplyr’
 
 # The following objects are masked from ‘package:stats’:
@@ -23,11 +26,14 @@ GO_data <- arrange(GO_data, GO_data[,3])
 GO_data$Term <- factor(GO_data$Term, levels = rev(GO_data$Term))
 
 mytheme <- theme(
-    axis.title = element_text(face = "bold", size = 14, colour = "black"),
-    axis.text = element_text(face = "bold", size = 14, colour = "black"), 
-    axis.line = element_line(size = 0.5, colour = "black"), 
+    axis.title = element_text(family = "Times_New_Roman", face = "bold", size = 25, colour = "black"),
+    axis.text = element_text(family = "Times_New_Roman", face = "bold", size = 25, colour = "black"), 
+    axis.line = element_line(size = 0.2, colour = "black"), 
     panel.background = element_rect(colour = "black"),
-    legend.key = element_blank()
+    legend.key = element_blank(),
+    legend.title = element_text(family = "Times_New_Roman", face = "bold", size = 30),
+    legend.text = element_text(family = "Times_New_Roman", face = "bold", size = 25),
+    # legend.spacing.y = unit(0.5, "cm")  # 调整标尺与图形的距离
 )
 
 p <- ggplot(
@@ -36,18 +42,18 @@ p <- ggplot(
         x = Fold.Enrichment,
         y = Term, 
         colour = 1*log10(PValue),
-        size = Count
+        size = Counts
     ))+
     geom_point()+
-    scale_size(range = c(2, 8))+
+    scale_size(range = c(1, 4))+
     scale_colour_gradient(low = "blue", high = "red")+
     theme_bw()+
     ylab("Terms")+
     xlab("Fold.Enrichment")+
     labs(color = expression(-log[10](PValue)))+
-    theme(legend.title = element_text(margin = margin(r = 50)), axis.title.x = element_text(margin = margin(t = 20)))+
-    theme(axis.text.x = element_text(face = "bold", colour = "black", angle = 0, vjust = 1))
+    theme(legend.title = element_text(margin = margin(r = 30)), axis.title.x = element_text(margin = margin(t = 20)))+   # 图例与图像界面右边距
+    theme(axis.text.x = element_text(family = "Times_New_Roman", face = "bold", colour = "black", angle = 0, vjust = 1))
 plot <- p + mytheme
 plot
 
-ggsave(plot, filename = "GO.png", width = 15, height = 9, dpi = 300)
+ggsave(plot, filename = "GO.png", width = 7, height = 7, dpi = 300)
